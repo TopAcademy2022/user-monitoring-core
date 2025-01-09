b97ab684528c1dd8c2e2be025cbd08d8c68f8d80
TopAcademy2022 <popov_r_top@outlook.com>
2025-01-09 20:31:16 +0300

    Update linux build script.

--- build/build-linux.sh
+++ build/build-linux.sh
@@ -10,10 +10,11 @@ CORE_LIB_NAME="user-monitoring-core.so"
 
 eval $CMAKE_PRE_BUILD_COMMAND
 eval $CMAKE_BUILD_COMMAND
-eval mv $CORE_OLD_LIB_NAME $CORE_LIB_NAME
+mv $CORE_OLD_LIB_NAME $CORE_LIB_NAME
 
 if [ -f "$CORE_LIB_NAME" ]; then
     echo "Core library exists"
+    exit 1
 else
     cp "$CORE_LIB_NAME" ../../
 fi
