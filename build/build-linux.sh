c049c20c5fd4e703b602a23b8651d5e218072d8f
TopAcademy2022 <popov_r_top@outlook.com>
2025-01-10 20:10:19 +0300

    Added build types to build script. Closed #10

--- build/build-linux.sh
+++ build/build-linux.sh
@@ -1,20 +1,50 @@
 #!/bin/bash
 
-mkdir project
-cd project || exit
+# Print all information on console
+set -x  # Enable debugging output
 
-CMAKE_PRE_BUILD_COMMAND="cmake ../../"
-CMAKE_BUILD_COMMAND="cmake --build ."
-CORE_OLD_LIB_NAME="libuser-monitoring-core.so"
-CORE_LIB_NAME="user-monitoring-core.so"
+# Save start directory
+START_DIR=$(pwd)
 
-eval $CMAKE_PRE_BUILD_COMMAND
-eval $CMAKE_BUILD_COMMAND
-mv $CORE_OLD_LIB_NAME $CORE_LIB_NAME
+# Set default exit strategy (AfterLastCommand or none)
+EXIT_STRATEGY="AfterLastCommand"
 
-if [ -f "$CORE_LIB_NAME" ]; then
-    cp "$CORE_LIB_NAME" ../../
+# Set build type (Release or Debug)
+BUILD_TYPE="Release"
+
+# Set build directory
+BUILD_DIR="project"
+
+# Run cmake build for library
+CMAKE_PRE_BUILD_COMMAND="cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ../../"
+CMAKE_BUILD_COMMAND="cmake --build . --config ${BUILD_TYPE}"
+CORE_LIB_NAME="user-monitoring-core.dll"
+
+# Check for the first argument
+if [ -z "\$1" ]; then
+    echo "Exit strategy is default = ${EXIT_STRATEGY}"
 else
-    echo "Core library exists"
+    EXIT_STRATEGY="\$1"
+fi
+
+# Create build directory
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
+
+# Execute cmake commands
+eval "$CMAKE_PRE_BUILD_COMMAND"
+eval "$CMAKE_BUILD_COMMAND"
+
+cd "$BUILD_TYPE" || exit 1
+
+# Check if the core library exists and handle accordingly
+if [ -e "$CORE_LIB_NAME" ]; then
+    cp "$CORE_LIB_NAME" "$START_DIR"
+
+    if [ "$EXIT_STRATEGY" == "AfterLastCommand" ]; then
+        exit 0
+    fi
+else
+    echo "Core library not exist"
     exit 1
 fi
